4028d1a3f0f3080007092d626041461351748a60
Ocean Bhardwaj <63634357+oceanbhardwaj@users.noreply.github.com>
2020-09-11 17:21:52 +0530

    Add files via upload

---
+++ compareVersionNumber.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int compareVersion(String version1, String version2) {
+        String s1[]=version1.split("\\.");
+        String s2[]=version2.split("\\.");
+        int max=Math.max(s1.length,s2.length);
+        for(int i=0;i<max;i++)
+        {
+            int v1=i<s1.length?Integer.parseInt(s1[i]):0;
+            int v2=i<s2.length?Integer.parseInt(s2[i]):0;
+            if(v1>v2)
+                return 1;
+            else if(v2>v1)
+                return -1;
+            else
+                continue;
+        }
+        
+       return 0; 
+    }
+}
