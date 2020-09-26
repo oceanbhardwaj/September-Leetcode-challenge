fea095062e5564c4586cbdabcd029d6375ea7ee2
Ocean Bhardwaj <63634357+oceanbhardwaj@users.noreply.github.com>
2020-09-26 23:43:01 +0530

    Add files via upload

---
+++ TeemoAttacking.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int findPoisonedDuration(int[] timeSeries, int duration) {
+        if(timeSeries.length==0)
+            return 0;
+        int total=0;
+        for(int i=0;i<timeSeries.length-1;i++)
+        {
+            total+=Math.min(timeSeries[i+1]-timeSeries[i],duration);
+        }
+        return total+duration;
+    }
+}
